82b238e04466d61d044cd5311fcc71fb7352d317
배지영 <bjy3392@naver.com>
2021-01-04 16:23:49 +0900

    vo생성

---
+++ bae2020/src/main/java/com/spring/bae2020/vo/ItemVo.java
@@ -0,0 +1,53 @@
+package com.spring.bae2020.vo;
+
+public class ItemVo {
+	private String item_idx;
+	private String order_idx;
+	private String product;
+	private String options;
+	private String price;
+	private String cnt;
+	public String getItem_idx() {
+		return item_idx;
+	}
+	public String getOrder_idx() {
+		return order_idx;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public String getOptions() {
+		return options;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public String getCnt() {
+		return cnt;
+	}
+	public void setItem_idx(String item_idx) {
+		this.item_idx = item_idx;
+	}
+	public void setOrder_idx(String order_idx) {
+		this.order_idx = order_idx;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	public void setOptions(String options) {
+		this.options = options;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public void setCnt(String cnt) {
+		this.cnt = cnt;
+	}
+	@Override
+	public String toString() {
+		return "ItemVo [item_idx=" + item_idx + ", order_idx=" + order_idx + ", product=" + product + ", options="
+				+ options + ", price=" + price + ", cnt=" + cnt + "]";
+	}
+	
+	
+}
